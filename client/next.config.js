cc9f4e67ea2710f0b2a124157092cffb99119866
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-05-20 16:49:34 +0200

    [new] Hardhat configuration + environment variables

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    INFURA_IPFS_PROJECT_ID: process.env.INFURA_IPFS_PROJECT_ID,
+    INFURA_IPFS_PROJECT_SECRET: process.env.INFURA_IPFS_PROJECT_SECRET,
+    INFURA_API_KEY: process.env.INFURA_API_KEY,
+    NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- server/src/hardhat.config.ts
+++ server/src/hardhat.config.ts
@@ -1,7 +1,29 @@
+import * as dotenv from "dotenv";
+dotenv.config({
+  path: `../.env`,
+});
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { readFileSync } from "fs";
+
+const privateKey = readFileSync(".secret").toString();
+const projectId = process.env.INFURA_API_KEY;
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    polygon_mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+    polygon_mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey], //Accounts from which we are deploying our contracts
+    },
+  },
   solidity: "0.8.18",
 };
 
