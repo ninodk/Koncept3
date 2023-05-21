09960577e1ddf5ffd20b1c5eb5d1b505459e28c6
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-05-21 22:14:39 +0200

    [upd] one last update to the Page Layout

--- client/src/components/Layout/PageLayout.tsx
+++ client/src/components/Layout/PageLayout.tsx
@@ -5,7 +5,7 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col w-full h-full">
       <Header />
-      <div className="flex flex-col w-full h-full">
+      <div className="flex flex-col w-full h-full min-h-screen">
         <div className="mx-6 my-16">{children}</div>
       </div>
       <Footer />
