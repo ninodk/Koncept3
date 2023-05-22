15ebe1eb2fd3dddb35ae5dc3c262843c8f69542a
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-05-22 05:51:06 +0200

    [new] Collections Page

--- client/src/pages/collections/index.tsx
+++ client/src/pages/collections/index.tsx
@@ -1,4 +1,20 @@
+import Link from "next/link";
+
 const CollectionsPage: React.FC = () => {
-  return <p>Here comes the NFTs created by the brand</p>;
+  return (
+    <div className="w-full max-h-screen">
+      <div className="flex justify-start">
+        <Link
+          href="/collections/create-nft"
+          className="px-4 py-1 text-white rounded-full bg-primary hover:bg-hoverPrimary"
+        >
+          Create NFT
+        </Link>
+      </div>
+      <div className="pt-5">
+        <div className="text-3xl ">No NFTs yet.</div>
+      </div>
+    </div>
+  );
 };
 export default CollectionsPage;
