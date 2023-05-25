c1cac228f25565316fee22f7b306bcac27b784cf
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-05-25 13:12:56 +0200

    [new] Create Event Page (Component)

--- client/src/pages/events/index.tsx
+++ client/src/pages/events/index.tsx
@@ -1,4 +1,20 @@
+import Link from "next/link";
+
 const EventsPage: React.FC = () => {
-  return <p>Here comes the events created by the brand</p>;
+  return (
+    <div className="w-full max-h-screen">
+      <div className="flex justify-start">
+        <Link
+          href="/events/create-event"
+          className="px-4 py-1 text-white rounded-full bg-primary hover:bg-hoverPrimary"
+        >
+          Create Event
+        </Link>
+      </div>
+      <div className="pt-5">
+        <div className="text-3xl ">No Events yet.</div>
+      </div>
+    </div>
+  );
 };
 export default EventsPage;
