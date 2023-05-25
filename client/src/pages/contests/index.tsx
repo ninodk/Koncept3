46fdc7a7eacbc01d68fd464d8ea8ece2a94fd4f1
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-05-25 23:56:04 +0200

    [new] Create Contest

--- client/src/pages/contests/index.tsx
+++ client/src/pages/contests/index.tsx
@@ -1,4 +1,20 @@
+import Link from "next/link";
+
 const ContestsPage: React.FC = () => {
-  return <p>Here comes the Contests created by the brand</p>;
+  return (
+    <div className="w-full max-h-screen">
+      <div className="flex justify-start">
+        <Link
+          href="/contests/create-contest"
+          className="px-4 py-1 text-white rounded-full bg-primary hover:bg-hoverPrimary"
+        >
+          Create Contest
+        </Link>
+      </div>
+      <div className="pt-5">
+        <div className="text-3xl ">No Contests yet.</div>
+      </div>
+    </div>
+  );
 };
 export default ContestsPage;
