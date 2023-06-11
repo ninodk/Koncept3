e489e962f345067519722c0d8ef60b684e823b20
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-06-11 03:48:29 +0200

    [new] Deploying Contracts to Hardhat Local Test Network

---
+++ server/src/configurations/config.js
@@ -0,0 +1,8 @@
+
+    export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+    export const storeContractAddress = "0x1613beB3B2C4f22Ee086B2b38C1476A3cE7f78E8"
+    export const NftContractAddress = "0x851356ae760d987E095750cCeb3bC6014560891C"
+    export const konTokenContractAddress = "0xf5059a5D33d5853360D16C683c16e67980206f36"
+    export const contestContractAddress = "0x95401dc811bb5740090279Ba06cfA8fcF6113778"
+    export const eventContractAddress = "0x998abeb3E57409262aE5b751f60747921B33613E"
+    
