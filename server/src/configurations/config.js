50b99300c50ef9ae268c9080285f178efa848989
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-08-08 23:55:39 +0200

    some dummy data

--- server/src/configurations/config.js
+++ server/src/configurations/config.js
@@ -1,8 +1,8 @@
 
-    export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-    export const storeContractAddress = "0x1613beB3B2C4f22Ee086B2b38C1476A3cE7f78E8"
-    export const NftContractAddress = "0x851356ae760d987E095750cCeb3bC6014560891C"
-    export const konTokenContractAddress = "0xf5059a5D33d5853360D16C683c16e67980206f36"
-    export const contestContractAddress = "0x95401dc811bb5740090279Ba06cfA8fcF6113778"
-    export const eventContractAddress = "0x998abeb3E57409262aE5b751f60747921B33613E"
+    export const ownerAddress = "0x121304beAE17c42d56BEB558E2D45344Cf6BCa46"
+    export const storeContractAddress = "0x05c3836b0Ab7078de45089708b4740E114fEE384"
+    export const NftContractAddress = "0x0BcB2de125CE6724E069A7b7507c4A2F921317C7"
+    export const konTokenContractAddress = "0x46619C8A1d1a571981BaD44eA5E08Fc0dFc5af8A"
+    export const contestContractAddress = "0x4D259730bC4A51cA81a470540eEb90f543381f33"
+    export const eventContractAddress = "0x8D7a06e109DFa38337bc9cF17E3a32E3E47FADbd"
     
