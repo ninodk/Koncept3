43563239f3e3ccccd8f24071eb1a807a1ee54853
Nino De Kerpel <nino.dekerpel@student.hogent.be>
2023-08-19 03:39:30 +0200

    [upd] new deployement on the mumbai polygon
    When a Smart Contract gets altered we need to deploy it again

--- server/src/configurations/config.js
+++ server/src/configurations/config.js
@@ -1,8 +1,8 @@
 
     export const ownerAddress = "0x121304beAE17c42d56BEB558E2D45344Cf6BCa46"
-    export const storeContractAddress = "0x05c3836b0Ab7078de45089708b4740E114fEE384"
-    export const NftContractAddress = "0x0BcB2de125CE6724E069A7b7507c4A2F921317C7"
-    export const konTokenContractAddress = "0x46619C8A1d1a571981BaD44eA5E08Fc0dFc5af8A"
-    export const contestContractAddress = "0x4D259730bC4A51cA81a470540eEb90f543381f33"
-    export const eventContractAddress = "0x8D7a06e109DFa38337bc9cF17E3a32E3E47FADbd"
+    export const storeContractAddress = "0x46C73FB66d473a3DFb3F98b2e1FD6C8a4534740F"
+    export const NftContractAddress = "0xc849dc4Bd91f098B0b20ebe3C703CdBd047ab827"
+    export const konTokenContractAddress = "0x2b81df6715bbb852B78E0086Bb2C4a545f5955C7"
+    export const contestContractAddress = "0x97AdAf791C22aFA845c11D7B2eC1fFCd857E1149"
+    export const eventContractAddress = "0x1Ca695e860f1796d8F555641c6E28095AF6e33a6"
     
